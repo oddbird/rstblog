bbf5e5d256541be1dc860a077dff012f22cbd843
Jonny Gerig Meyer <jonny@oddbird.net>
2017-02-28 15:17:25 -0500

    Bump OBC version.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='rstblog',
-    version='1.0.obc2',
+    version='1.0.obc3',
     author='Armin Ronacher <armin.ronacher@active-4.com>',
     packages=['rstblog', 'rstblog.modules'],
     include_package_data=True,
